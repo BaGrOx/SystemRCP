703a8aad046422d1f7caefcf16831f94bc84348e
Kuba <bagro2803@gmail.com>
2022-12-28 14:00:17 +0100

    Program for recording employees' working time

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿
+using SystemRCP_CSV;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        try
+        {
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            var rcp1Files = Directory.GetFiles(currentDirectory, "rcp1*.csv");
+
+            var rcp2Files = Directory.GetFiles(currentDirectory, "rcp2*.csv");
+
+
+
+            var employees1 = new List<WorkDay>();
+            foreach (var file in rcp1Files)
+            {
+                employees1 = ReadFile.ReadFile1(file);
+            }
+
+            var employees2 = new List<WorkDay>();
+            foreach (var file in rcp2Files)
+            {
+                employees2 = ReadFile.ReadFile2(file);
+
+            }
+
+            var employees = employees1.Concat(employees2).ToList();
+
+            employees = employees.OrderBy(x => x.EmployeeCode).ThenBy(x => x.Date).ToList();
+
+            var workdays = new List<WorkDay>();
+
+            foreach (var employee in employees)
+            {
+                var workday = workdays.FirstOrDefault(x => x.EmployeeCode == employee.EmployeeCode && x.Date == employee.Date);
+                if (workday == null)
+                {
+                    workdays.Add(employee);
+                }
+            }
+
+            foreach (var employee in employees)
+            {
+                employees.OrderBy(x => x.EmployeeCode).ToList();
+                Console.WriteLine($"Pracownik {employee.EmployeeCode}:");
+                if (!(employee.EndTime < employee.StartTime))
+                    Console.WriteLine($"{employee.Date:yyyy-MM-dd}: {employee.StartTime:hh\\:mm} - {employee.EndTime:hh\\:mm} ({employee.EndTime - employee.StartTime:hh\\:mm})\n");
+                else
+                {
+                    employee.EndTime = employee.EndTime.Add(new TimeSpan(24, 0, 0));
+                    Console.WriteLine($"{employee.Date:yyyy-MM-dd}: {employee.StartTime:hh\\:mm} - {employee.EndTime:hh\\:mm} ({employee.EndTime - employee.StartTime:hh\\:mm})\n");
+                }
+
+            }
+
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine("Brak uprawnień do odczytu plików." + ex.Message);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine("Katalog nie został odnaleziony." + ex.Message);
+        }
+    }
+
+}
---
+++ ReadFile.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemRCP_CSV
+{
+    public  class ReadFile
+    {
+        public static List<WorkDay> ReadFile1(string fileName)
+        {
+            var employees = new List<WorkDay>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    var parts = line.Split(';');
+                    if (parts.Length < 5) continue;
+
+                    var employeeCode = parts[0];
+                    var date = DateTime.Parse(parts[1]);
+                    var startTime = TimeSpan.Parse(parts[2]);
+                    var endTime = TimeSpan.Parse(parts[3]);
+
+                    employees.Add(new WorkDay
+                    {
+                        EmployeeCode = employeeCode,
+                        Date = date,
+                        StartTime = startTime,
+                        EndTime = endTime
+                    });
+                }
+            }
+
+            return employees;
+        }
+
+
+        public static List<WorkDay> ReadFile2(string fileName)
+        {
+            var employees = new List<WorkDay>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(';');
+                    if (parts.Length < 4) continue;
+
+                    if (!parts.Contains(string.Empty))
+                    {
+                        var employeeCode = parts[0];
+                        var date = DateTime.Parse(parts[1]);
+                        var time = TimeSpan.Parse(parts[2]);
+                        var type = parts[3];
+
+                        var workday = employees.FirstOrDefault(x => x.EmployeeCode == employeeCode && x.Date == date);
+                        if (workday == null)
+                        {
+                            if (type == "WE")
+                            {
+                                workday = new WorkDay
+                                {
+                                    EmployeeCode = employeeCode,
+                                    Date = date,
+                                    StartTime = time
+                                };
+                                employees.Add(workday);
+                            }
+                        }
+                        else
+                        {
+                            if (type == "WY")
+                            {
+                                workday.EndTime = time;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return employees;
+        }
+    }
+}
